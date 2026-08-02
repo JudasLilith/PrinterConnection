8c77b232e0baec1dc6e15712f142cee0cfc1b4c0
Myeongjae Seo <peterseo1004@icloud.com>
2026-08-01 22:50:58 -0500

    added blessed, could finally print to page

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -15,6 +15,7 @@ import { execSync } from 'child_process';
 
 import * as readline from "node:readline/promises";
 
+import blessed from 'blessed';
 
 /*
 import { ping } from '@network-utils/tcp-ping'
@@ -54,17 +55,14 @@ try {
 
     '--init': Boolean,
     '-i': Boolean,
+
   });
+
+  console.log(chalk.bgCyanBright(chalk.black("starting the tool.....")));
+
   //console.log(args);
   const config = getConfig();
 
-  console.log(config);
-
-
-
-  start(config);
-
-  console.log(chalk.bgCyanBright(chalk.black("starting the tool.....")));
 
 
 
@@ -76,8 +74,52 @@ try {
     if (checkTailscale === 'Command failed: tailscale status --peers=false') {
       console.log(chalk.red("tailscale is not on; turning on......"));
       execSync('tailscale up');
-
     }
+    var screen = blessed.screen({
+      smartCSR: true
+    });
+
+    screen.title = 'my window title';
+
+    // Create a box perfectly centered horizontally and vertically.
+    var box = blessed.box({
+      top: 'center',
+      left: 'center',
+      width: '50%',
+      height: '50%',
+      content: 'Hello {bold}world{/bold}!',
+      tags: true,
+      border: {
+        type: 'line'
+      },
+      style: {
+        fg: 'white',
+        bg: 'magenta',
+        border: {
+          fg: '#f0f0f0'
+        },
+        hover: {
+          bg: 'green'
+        }
+      }
+    });
+
+    // Append our box to the screen.
+    screen.append(box);
+
+
+
+    // Quit on Escape, q, or Control-C.
+    screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+      return process.exit(0);
+    });
+
+    // Focus our element.
+    box.focus();
+
+    // Render the screen.
+    screen.render();
+
     console.log()
 
 
@@ -138,12 +180,7 @@ try {
         console.log(chalk.red("wrong input!"));
       }
     }
-    saveConfig('remove', `ip = ${answer}`);
-    getConfig();
-
-
-
-
+    saveConfig('append', `ip = ${answer}`);
 
   }
 
--- tool/src/commands/config-mgr.js
+++ tool/src/commands/config-mgr.js
@@ -6,6 +6,8 @@ import { cosmiconfigSync } from 'cosmiconfig';
 
 import fs from "fs";
 
+import ini from 'ini';
+
 import path from "path";
 
 import { fileURLToPath } from 'url';
@@ -19,29 +21,14 @@ const configPath = path.join(__dirname, '../../../config/PrinterConnection.conf'
 
 
 export default function getConfig() {
-  let config = fs.readFileSync(configPath, 'utf8', (err, data) => {
-    if (err) {
-      console.log(chalk.red('error reading:', err));
-      return;
-    }
-    let lines = config.split('\n');
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i].trim();
-
-    }
-
-    console.log(data);
-  });
-
-  console.log(config);
-
-
-
-
-
+  let config = ini.parse(fs.readFileSync(configPath, 'utf8'));
+  console.log(config.ip)
+  fs.writeFileSync(configPath, ini.stringify(config));
 }
 
 export function saveConfig(status, value) {
+  let config = ini.parse(fs.readFileSync(configPath, 'utf8'));
+
   if (status === 'add') {
     fs.writeFileSync(configPath, value);
   }
@@ -53,5 +40,9 @@ export function saveConfig(status, value) {
     fs.writeFileSync(configPath, value);
     console.log(configPath, typeof configPath);
   }
+
+
+  fs.writeFileSync(configPath, ini.stringify(config));
+
 
 }
